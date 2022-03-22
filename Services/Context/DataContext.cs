df6bdd63bd5d7e7c5a404e5de870e2ec23044385
alsalmiw <alsalmiw@gmail.com>
2022-03-22 13:52:51 -0700

    added tables to Data context and user controller

--- Services/Context/DataContext.cs
+++ Services/Context/DataContext.cs
@@ -1,12 +1,23 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.Emit;
 using System.Threading.Tasks;
+using CodewarsBack.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace CodewarsBack.Services.Context
 {
-    public class DataContext
+    public class DataContext: DbContext
     {
-        
+        public DbSet<UserModel>UserInfo{get;set;}
+        public DbSet<ReserveModel> ReserveInfo {get; set;}
+
+        public DataContext (DbContextOptions options) : base (options) {}
+
+        protected override void onModelCreating(ModuleBuilder builder)
+        {
+            base.onModelCreating(builder);
+        }
     }
 }
