88861035f8f51648f69371aa045a67117406febd
alsalmiw <alsalmiw@gmail.com>
2022-03-22 13:04:45 -0700

    added cors policy

--- Program.cs
+++ Program.cs
@@ -2,6 +2,15 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+
+builder.Services.AddCors(options => {
+options.AddPolicy("CodeWarsPolicy",
+builder => {builder.WithOrigins("http://localhost:5262")
+    .AllowAnyHeader()
+    .AllowAnyMethod();
+});
+});
+
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -17,6 +26,8 @@ if (app.Environment.IsDevelopment())
 }
 
 //app.UseHttpsRedirection();
+
+app.UseCors("CodeWarsPolicy");
 
 app.UseAuthorization();
 
